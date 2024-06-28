e7315e35c23a662fb2eb01f3db8c25f4cf913eb4
Kammachi <126817246+Kammachi@users.noreply.github.com>
2024-06-28 20:52:32 +0200

    Add winner checking algorithm

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 const Game = (function () {
 
     const Gameboard = {
-        board: ['', 'X', '', 'X', 'O', 'O', '', '', ''], 
+        board: [['', 'X', ''], ['X', 'O', 'O'], ['', '', '']], 
 
         resetBoard: function() {
-            this.board = ['', '', '', '', '', '', '', '', ''];
+            this.board = [['', '', ''], ['', '', ''], ['', '', '']];
+        },
+        
+        checkWin: function() {
+            board = this.board;
+            
+
+            for (let i = 0; i < 3; i++) {
+                /* Check horizontally */
+
+                if (["OOO", "XXX"].includes(board[i].join(''))) {
+                    return true;
+                } 
+
+                /* Check vertically */
+
+                if (["OOO", "XXX"].includes(
+                    board[0][i] + board[1][i] + board[2][i])) {
+                    return true;
+                }
+            }
+
+            if (["OOO", "XXX"].includes(
+                board[0][0] + board[1][1] + board[2][2]) || 
+                ["OOO", "XXX"].includes(
+                board[0][2] + board[1][1] + board[2][0])) {
+
+                return true;
+            }
+
+            return false;
         },
 
         render: function () {
             const gameContainer = document.querySelector('.gameContainer');
 
-            for (let position of this.board) {
-                const gameSquare = document.createElement('div');
-                
-                gameSquare.classList.add('gameSquare');
-                gameSquare.textContent = position;
+            for (let row of this.board) {
+                for (let cell of row) {
+                    const gameSquare = document.createElement('div');
+                    
+                    gameSquare.classList.add('gameSquare');
+                    gameSquare.textContent = cell;
 
-                gameContainer.appendChild(gameSquare);
+                    gameContainer.appendChild(gameSquare);
+                }
             }
         }
     }
@@ -48,18 +80,23 @@ const Game = (function () {
         while (running) {
             // render()
 
-            const playerChoice = prompt('Input a number of a square:');
+            const playerChoiceX = prompt('x coordinate:');
+            const playerChoiceY = prompt('y coordinate:');
 
-            Gameboard.board[playerChoice] = sign;
+            Gameboard.board[playerChoiceX][playerChoiceY] = sign;
 
             sign = sign === 'X' ? 'O' : 'X';
 
-            running = false;
+            if (Gameboard.checkWin()) {
+                running = false;
+            }
+
         }
     }
 
     newGame();
 
+    return Gameboard.board;
 })();
 
 
